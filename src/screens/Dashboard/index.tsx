9d07bb6eb0d1c841bf2fd8a652ff6c40b0ce1b7f
ArlanBiati <arlangustavo@hotmail.com>
2022-01-12 19:52:37 -0300

    feat: implementação dos cartões de destaque

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -32,9 +32,26 @@ export function Dashboard(){
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          title='Entradas'
+          amount='R$ 17.000,00'
+          lastTransaction='Última entrada dia 13 de abril.'
+          type='up'
+        />
+
+        <HighlightCard
+          title='Saídas'
+          amount='R$ 7.000,00'
+          lastTransaction='Última saída dia 13 de abril.'
+          type='down'
+        />
+
+        <HighlightCard
+          title='Total'
+          amount='R$ 10.000,00'
+          lastTransaction='01 à 16 de abril.'
+          type='total'
+        />
       </HighlightCards>
 
     </Container>
